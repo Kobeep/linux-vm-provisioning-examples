0b2b971d89301f6589de54b9330e5e99738a3f0b
Kobeep <kuba.pospieszny@gmail.com>
2024-07-28 20:55:45 +0200

    #9Clean yaml

--- .github/ansible.sh
+++ .github/ansible.sh
@@ -4,5 +4,6 @@ which python3
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt update
 sudo apt install ansible
-
+python3 -m pip install --user ansible
+python3 -m pip install --user ansible-core
 #test
