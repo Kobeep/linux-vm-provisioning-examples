eab744531c4ec51d82f6a8863be632568d8c77b8
Kobeep <kuba.pospieszny@gmail.com>
2024-07-28 20:09:40 +0200

    Clean yaml

---
+++ .github/ansible.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+sudo apt install python3
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt update
+sudo apt install ansible
---
+++ .github/vagrant.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt install vagrant
+sudo cp /etc/apt/sources.list /etc/apt/sources.list."$(date +"%F")"
+sudo sed -i -e '/^# deb-src.*universe$/s/# //g' /etc/apt/sources.list
+sudo apt-get -y update
+sudo apt-get -y install nfs-kernel-server
+sudo systemctl enable --now nfs-server
+sudo apt-get -y build-dep vagrant ruby-libvirt
+sudo apt-get -y install ebtables dnsmasq-base
+sudo apt-get -y install libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev
+vagrant plugin install vagrant-libvirt
